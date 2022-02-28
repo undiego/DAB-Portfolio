0b156d59dcf360a281d6786ca3ed63ae6327d719
Diego Barrio <barriodiego@gmail.com>
2022-02-28 18:16:14 -0300

    Modificaciones en header + login component

---
+++ dab-portfolio/src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { DatosPortfolioService } from 'src/app/services/datos-portfolio.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  dataPortfolio:any;
+
+  constructor(private datosPortfolioService: DatosPortfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPortfolioService.getDatos().subscribe(data => {this.dataPortfolio = data});
+  }
+
+}
