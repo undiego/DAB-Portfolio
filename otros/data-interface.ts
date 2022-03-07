482675d7af533080ceee32eb9cdc587c19c6f6d8
Diego <barriodiego@gmail.com>
2022-03-06 21:26:05 -0300

    No levanta la info de db.json

---
+++ otros/data-interface.ts
@@ -0,0 +1,38 @@
+export interface Data {
+    "logoArgentinaPrograma": string,
+		"textoArgentinaPrograma": string,
+		"textLogin": string,
+		"name":string,
+		"backImage": string,
+		"photo": string,
+		"position": string,
+    	"location": string,
+		"alertText": string,
+		"about": {
+		"title": string,
+		"text": string
+		},
+		"done": {
+		"title": string,
+		"text": string,
+		"url": string
+		},
+		"workExperience": {
+		"title": string,
+		"text": string
+		}
+		"computerSkills": {
+		"title": string,
+		"text": string,
+		},
+		"education": {
+		"title": string,
+		"text": string,
+		},
+		"otherSkills": {
+		"title": string,
+		"text": string,
+		},
+		"textBtnAddSection": string,
+		"textFooter": string
+}
